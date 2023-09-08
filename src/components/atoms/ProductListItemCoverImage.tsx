9742fb278301ce3dbd928c175584ac55c402e3e8
Jakub <jchrobakk@gmail.com>
2023-09-08 17:38:11 +0200

    feat: ProductListItemCoverImage

---
+++ src/components/atoms/ProductListItemCoverImage.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+type ProductListItemCoverImage = {
+	src: string;
+	alt: string;
+	size?: number;
+};
+
+export const ProductListItemCoverImage = ({ src, alt, size = 320 }: ProductListItemCoverImage) => {
+	return (
+		<div className="inline-block aspect-square bg-slate-50 p-4 transition-colors hover:bg-slate-100">
+			<Image
+				alt={alt}
+				src={src}
+				width={size}
+				height={size}
+				className="transition-transform hover:scale-105"
+			/>
+		</div>
+	);
+};
