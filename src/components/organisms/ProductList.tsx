0d606ff65089eb34f96a4bc675f9da8f1ea758d4
Jakub <jchrobakk@gmail.com>
2023-09-08 17:52:36 +0200

    test: add testid

--- src/components/organisms/ProductList.tsx
+++ src/components/organisms/ProductList.tsx
@@ -7,7 +7,10 @@ type ProductListItemProps = {
 
 export const ProductList = ({ products }: ProductListItemProps) => {
 	return (
-		<ul className="mb-8 mt-4 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
+		<ul
+			className="mb-8 mt-4 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4"
+			data-testid="products-list"
+		>
 			{products.map((product) => (
 				<ProductListItem key={product.id} product={product} />
 			))}
