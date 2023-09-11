3660c27d999dcf50bd2bb798eef0e672bdbfa820
Jakub <jchrobakk@gmail.com>
2023-09-11 13:18:12 +0200

    feat: add static pages dir & faq and tos placeholder pages

---
+++ src/app/(static-pages)/faq/page.tsx
@@ -0,0 +1,12 @@
+export default function FrequentlyAskedQuestions() {
+	return (
+		<>
+			<h1 className="text-4xl">FAQ</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, tenetur dolore! Incidunt
+				delectus cupiditate repudiandae assumenda voluptatibus ipsum eum rerum debitis placeat, nisi
+				tenetur optio officiis suscipit tempore, autem consequuntur.
+			</p>
+		</>
+	);
+}
---
+++ src/app/(static-pages)/layouts.tsx
@@ -0,0 +1,3 @@
+export default function StaticPagesLayout({ children }: { children: React.ReactNode }) {
+	return <div className="mx-auto max-w-md text-center">{children}</div>;
+}
