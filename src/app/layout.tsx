c05aeef8ecf80f4a52361175ab48342d6d9a0f28
Jakub <jchrobakk@gmail.com>
2023-09-11 13:10:04 +0200

    refactor: move wrapper to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<section className="sm:py-18 mx-auto flex w-full max-w-2xl flex-grow flex-col px-8 py-12 sm:px-6 lg:max-w-7xl">
+					{children}
+				</section>
+			</body>
 		</html>
 	);
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -36,9 +36,5 @@ export default function Products() {
 		},
 	];
 
-	return (
-		<main className="sm:py-18 mx-auto flex w-full max-w-2xl flex-grow flex-col px-8 py-12 sm:px-6 lg:max-w-7xl">
-			<ProductList products={products} />
-		</main>
-	);
+	return <ProductList products={products} />;
 }
