a95d0eb2740730f5a9ae495f1aec2a08bc41e5ce
Jakub <jchrobakk@gmail.com>
2023-09-12 11:14:21 +0200

    feat: add link to products in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-	return <h1 className="text-2xl">Main page</h1>;
+	return (
+		<>
+			<h1 className="text-2xl">Main page</h1>
+			<Link href="/products" className="text-blue-500 hover:underline">
+				Products
+			</Link>
+		</>
+	);
 }
