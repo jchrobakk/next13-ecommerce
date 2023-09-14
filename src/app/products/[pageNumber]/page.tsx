352ba7c96aaf928a2121f2f4562d7a95e3c06ae3
Jakub <jchrobakk@gmail.com>
2023-09-14 17:41:51 +0200

    feat: Pagination

--- src/app/products/[pageNumber]/page.tsx
+++ src/app/products/[pageNumber]/page.tsx
@@ -1,8 +1,14 @@
 import { getProductsByPage } from "@/api/products";
 import { ProductList } from "@/components/organisms/ProductList";
+import { Pagination } from "@/components/molecules/Pagination";
 
 export default async function ProductsPage({ params }: { params: { pageNumber: string } }) {
 	const products = await getProductsByPage(+params.pageNumber);
 
-	return <ProductList products={products} />;
+	return (
+		<>
+			<ProductList products={products} />
+			<Pagination currentPage={+params.pageNumber} className="mt-4" />
+		</>
+	);
 }
