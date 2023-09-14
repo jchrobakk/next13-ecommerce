99977355f5bf7534dd613fb526f8f959bf47834c
Jakub <jchrobakk@gmail.com>
2023-09-14 15:48:30 +0200

    fix: ProductList margin

--- src/components/organisms/ProductList.tsx
+++ src/components/organisms/ProductList.tsx
@@ -8,7 +8,7 @@ type ProductListItemProps = {
 export const ProductList = ({ products }: ProductListItemProps) => {
 	return (
 		<ul
-			className="mb-8 mt-4 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4"
+			className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4"
 			data-testid="products-list"
 		>
 			{products.map((product) => (
