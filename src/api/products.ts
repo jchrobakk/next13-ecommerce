947517d616b8e6a87bd857575a5de78d147eacdd
Jakub <jchrobakk@gmail.com>
2023-09-20 18:04:02 +0200

    refactor: executeGraphql and transformProduct to utils

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,5 +1,6 @@
 import { type Product } from "@/components/types";
-import { ProductsGetListDocument, type TypedDocumentString } from "@/gql/graphql";
+import { ProductsGetListDocument } from "@/gql/graphql";
+import { transformProduct, executeGraphql } from "@/utils";
 
 type ProductResponse = {
 	id: string;
@@ -13,16 +14,7 @@ type ProductResponse = {
 export const getProductsList = async (): Promise<Product[]> => {
 	const graphqlResponse = await executeGraphql(ProductsGetListDocument, {});
 
-	return graphqlResponse.products.map((p) => {
-		return {
-			id: p.id,
-			name: p.name,
-			price: p.price,
-			description: p.description,
-			category: p.categories[0]?.name || "No Category",
-			image: p.images[0]?.url,
-		};
-	});
+	return graphqlResponse.products.map(transformProduct);
 };
 
 export const getProduct = async (id: string): Promise<Product> => {
@@ -32,15 +24,6 @@ export const getProduct = async (id: string): Promise<Product> => {
 
 	return transformProduct(productResponse);
 };
-
-const transformProduct = (product: ProductResponse): Product => ({
-	id: product.id,
-	name: product.name,
-	price: product.price,
-	description: product.description,
-	category: product.categories[0]?.name || "No Category",
-	image: product.images[0]?.url,
-});
 
 export const getProductsByPage = async (page: number): Promise<Product[]> => {
 	const productsPerPage = 8;
@@ -53,33 +36,4 @@ export const getProductsByPage = async (page: number): Promise<Product[]> => {
 	const productsResponse = (await res.json()) as ProductResponse[];
 
 	return productsResponse.map(transformProduct);
-};
-
-const executeGraphql = async <TResult, TVariables>(
-	query: TypedDocumentString<TResult, TVariables>,
-	variables: TVariables,
-): Promise<TResult> => {
-	if (!process.env.GRAPHQL_URL) {
-		throw TypeError("GRAPHQL_URL is not defined");
-	}
-
-	const res = await fetch(process.env.GRAPHQL_URL, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ query, variables }),
-	});
-
-	type GraphQLResponse<T> =
-		| { data: T; errors: undefined }
-		| { data?: undefined; errors: { message: string }[] };
-
-	const graphqlResponse = (await res.json()) as GraphQLResponse<TResult>;
-
-	if (graphqlResponse.errors) {
-		throw TypeError(`GraphQL Error`, { cause: graphqlResponse.errors });
-	}
-
-	return graphqlResponse.data;
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,56 @@
+import { type Product } from "./components/types";
+import { type TypedDocumentString } from "./gql/graphql";
+
+type ProductResponse = {
+	id: string;
+	name: string;
+	price: number;
+	description: string;
+	categories: { name: string }[];
+	images: { url: string }[];
+};
+
 export const formatPrice = (price: number) => {
 	return new Intl.NumberFormat("en-US", {
 		style: "currency",
 		currency: "USD",
 	}).format(price);
+};
+
+export const transformProduct = (product: ProductResponse): Product => ({
+	id: product.id,
+	name: product.name,
+	price: product.price,
+	description: product.description,
+	category: product.categories[0]?.name || "No Category",
+	image: product.images[0]?.url,
+});
+
+export const executeGraphql = async <TResult, TVariables>(
+	query: TypedDocumentString<TResult, TVariables>,
+	variables: TVariables,
+): Promise<TResult> => {
+	if (!process.env.GRAPHQL_URL) {
+		throw TypeError("GRAPHQL_URL is not defined");
+	}
+
+	const res = await fetch(process.env.GRAPHQL_URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ query, variables }),
+	});
+
+	type GraphQLResponse<T> =
+		| { data: T; errors: undefined }
+		| { data?: undefined; errors: { message: string }[] };
+
+	const graphqlResponse = (await res.json()) as GraphQLResponse<TResult>;
+
+	if (graphqlResponse.errors) {
+		throw TypeError(`GraphQL Error`, { cause: graphqlResponse.errors });
+	}
+
+	return graphqlResponse.data;
 };
