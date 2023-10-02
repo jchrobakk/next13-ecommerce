87d59cc66d4892bcb94de8d670680bf9b8cfcc36
Jakub <jchrobakk@gmail.com>
2023-10-02 17:51:39 +0200

    fix: webhook for revalidating products

--- src/app/api/webhook/hygraph/product/route.ts
+++ src/app/api/webhook/hygraph/product/route.ts
@@ -1,33 +1,12 @@
 import { revalidatePath } from "next/cache";
-import { NextResponse, type NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function POST(request: NextRequest): Promise<NextResponse> {
-	const json: unknown = await request.json();
+export async function POST(): Promise<NextResponse> {
+	revalidatePath(`/product/[productId]`, "page");
+	revalidatePath(`/products/[pageNumber]`, "page");
+	revalidatePath("/categories/[slug]/[pageNumber]", "page");
 
-	if (
-		typeof json === "object" &&
-		json &&
-		"productId" in json &&
-		typeof json.productId === "string"
-	) {
-		revalidatePath(`/product/[productId]`, "page");
-		revalidatePath(`/products/[pageNumber]`, "page");
-		revalidatePath("/categories/[slug]/[pageNumber]", "page");
+	console.log(`Revalidated products`);
 
-		console.log(`Revalidated product ${json.productId}`);
-
-		return NextResponse.json(
-			{ message: `Success. Product ${json.productId} revalidated.` },
-			{ status: 201 },
-		);
-	}
-
-	return NextResponse.json(
-		{
-			message: "Invalid body",
-		},
-		{
-			status: 400,
-		},
-	);
+	return NextResponse.json({ message: `Success. Products revalidated.` }, { status: 201 });
 }
