fd30640462605bc93fd22e7cd8168d3eab8b4c1f
Jakub <jchrobakk@gmail.com>
2023-10-02 18:31:22 +0200

    fix: revalidate only specific product

--- src/app/api/webhook/hygraph/product/route.ts
+++ src/app/api/webhook/hygraph/product/route.ts
@@ -1,12 +1,36 @@
 import { revalidatePath } from "next/cache";
-import { NextResponse } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
-export async function POST(): Promise<NextResponse> {
-	revalidatePath(`/product/[productId]`, "page");
-	revalidatePath(`/products/[pageNumber]`, "page");
-	revalidatePath("/categories/[slug]/[pageNumber]", "page");
+export async function POST(request: NextRequest): Promise<NextResponse> {
+	const json: unknown = await request.json();
 
-	console.log(`Revalidated products`);
+	if (
+		typeof json === "object" &&
+		json !== null &&
+		"data" in json &&
+		typeof json.data === "object" &&
+		json.data !== null &&
+		"id" in json.data &&
+		typeof json.data.id === "string"
+	) {
+		revalidatePath(`/product/${json.data.id}`, "page");
+		revalidatePath(`/products/[pageNumber]`, "page");
+		revalidatePath("/categories/[slug]/[pageNumber]", "page");
 
-	return NextResponse.json({ message: `Success. Products revalidated.` }, { status: 201 });
+		console.log(`Revalidated product ${json.data.id}`);
+
+		return NextResponse.json(
+			{ message: `Success. Product ${json.data.id} revalidated.` },
+			{ status: 201 },
+		);
+	}
+
+	return NextResponse.json(
+		{
+			message: "Invalid body",
+		},
+		{
+			status: 400,
+		},
+	);
 }
