cd550053b78e4bf1d9b97df178afb5cabb89d58f
Jakub <jchrobakk@gmail.com>
2023-10-02 17:43:56 +0200

    feat: add webhook for revalidating products

---
+++ src/app/api/webhook/hygraph/product/route.ts
@@ -0,0 +1,33 @@
+import { revalidatePath } from "next/cache";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+	const json: unknown = await request.json();
+
+	if (
+		typeof json === "object" &&
+		json &&
+		"productId" in json &&
+		typeof json.productId === "string"
+	) {
+		revalidatePath(`/product/${json.productId}`, "page");
+		revalidatePath(`/products/[pageNumber]`, "page");
+		revalidatePath("/categories/[slug]/[pageNumber]", "page");
+
+		console.log(`Revalidated product ${json.productId}`);
+
+		return NextResponse.json(
+			{ message: `Success. Product ${json.productId} revalidated.` },
+			{ status: 201 },
+		);
+	}
+
+	return NextResponse.json(
+		{
+			message: "Invalid body",
+		},
+		{
+			status: 400,
+		},
+	);
+}
