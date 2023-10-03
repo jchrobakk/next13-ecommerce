1d7f555ee8eebdfd06c251eb65924df9e6512bf3
Jakub <jchrobakk@gmail.com>
2023-10-03 11:38:27 +0200

    fix: add margin in cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -11,7 +11,7 @@ export default async function CartPage() {
 
 	return (
 		<section>
-			<h1 className="text-xl font-bold text-gray-900 sm:text-3xl">Your Cart</h1>
+			<h1 className="mb-8 text-xl font-bold text-gray-900 sm:text-3xl">Your Cart</h1>
 			<ul>
 				{cart.orderItems.map(
 					(item) => item.product && <ProductCartListItem order={item} key={item.product.id} />,
