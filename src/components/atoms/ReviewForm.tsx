ae5b394dfde35708a931029aed00b85390645f70
Jakub <jchrobakk@gmail.com>
2023-10-05 11:38:25 +0200

    test: adjust review form to tests

--- src/components/atoms/ReviewForm.tsx
+++ src/components/atoms/ReviewForm.tsx
@@ -1,7 +1,21 @@
 export const ReviewForm = () => {
 	return (
-		<form className="space-y-4 lg:col-span-3 lg:p-12" data-testid="test-review-form">
+		<form className="space-y-4 lg:col-span-3 lg:p-12" data-testid="add-review-form">
 			<h2 className="text-lg font-medium text-gray-900">Write a review</h2>
+			<div>
+				<label className="sr-only" htmlFor="headline">
+					Review title
+				</label>
+
+				<input
+					required
+					className="w-full rounded-lg border-gray-200 p-3 text-sm"
+					placeholder="Review title"
+					type="text"
+					name="headline"
+					id="headline"
+				/>
+			</div>
 			<div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
 				<div>
 					<label className="sr-only" htmlFor="email">
@@ -12,18 +26,20 @@ export const ReviewForm = () => {
 						className="w-full rounded-lg border-gray-200 p-3 text-sm"
 						placeholder="Email address"
 						type="email"
+						name="email"
 						id="email"
 					/>
 				</div>
 				<div>
 					<label className="sr-only" htmlFor="name">
-						Name<section className="mt-4"></section>
+						Name
 					</label>
 					<input
 						required
 						className="w-full rounded-lg border-gray-200 p-3 text-sm"
 						placeholder="Name"
 						type="text"
+						name="name"
 						id="name"
 					/>
 				</div>
@@ -38,6 +54,7 @@ export const ReviewForm = () => {
 					required
 					className="w-full rounded-lg border-gray-200 p-3 text-sm"
 					placeholder="Rating"
+					name="rating"
 					id="rating"
 				>
 					<option value="1">1 star</option>
@@ -57,6 +74,7 @@ export const ReviewForm = () => {
 					className="w-full rounded-lg border-gray-200 p-3 text-sm"
 					placeholder="Content"
 					rows={8}
+					name="content"
 					id="content"
 				></textarea>
 			</div>
