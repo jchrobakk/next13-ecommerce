4ea657687001d400aa0293ca7979adfa2506fc70
Jakub <jchrobakk@gmail.com>
2023-10-06 10:40:37 +0200

    test: adjust navbar to testing

--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -6,42 +6,44 @@ export const Navbar = async () => {
 	const cart = await getCartFromCookies();
 	const quantity = cart?.orderItems.length || 0;
 	return (
-		<nav className="mx-auto flex max-w-4xl items-center justify-between gap-4 px-8 py-4 sm:px-6 lg:max-w-7xl">
-			<ul className="flex gap-4">
-				<li>
-					<ActiveLink href="/">Home</ActiveLink>
-				</li>
-				<li>
-					<ActiveLink exact={false} href="/products">
-						All
-					</ActiveLink>
-				</li>
-				<li>
-					<ActiveLink href="/categories">Categories</ActiveLink>
-				</li>
-				<li>
-					<ActiveLink href="/collections">Collections</ActiveLink>
-				</li>
-				<li>
-					<ActiveLink exact={false} href="/categories/t-shirts">
-						T-shirts
-					</ActiveLink>
-				</li>
-				<li>
-					<ActiveLink exact={false} href="/categories/hoodies">
-						Hoodies
-					</ActiveLink>
-				</li>
-				<li>
-					<ActiveLink exact={false} href="/categories/accessories">
-						Accessories
-					</ActiveLink>
-				</li>
-			</ul>
+		<div className="mx-auto flex max-w-4xl justify-between px-8 py-4 sm:px-6 lg:max-w-7xl">
+			<nav className="flex items-center  gap-4 ">
+				<ul className="flex gap-4">
+					<li>
+						<ActiveLink href="/">Home</ActiveLink>
+					</li>
+					<li>
+						<ActiveLink exact={false} href="/products">
+							All
+						</ActiveLink>
+					</li>
+					<li>
+						<ActiveLink href="/categories">Categories</ActiveLink>
+					</li>
+					<li>
+						<ActiveLink href="/collections">Collections</ActiveLink>
+					</li>
+					<li>
+						<ActiveLink exact={false} href="/categories/t-shirts">
+							T-shirts
+						</ActiveLink>
+					</li>
+					<li>
+						<ActiveLink exact={false} href="/categories/hoodies">
+							Hoodies
+						</ActiveLink>
+					</li>
+					<li>
+						<ActiveLink exact={false} href="/categories/accessories">
+							Accessories
+						</ActiveLink>
+					</li>
+				</ul>
+			</nav>
 			<div className="flex items-center gap-4">
 				<Search />
 				<ActiveLink href="/cart">Cart ({quantity})</ActiveLink>
 			</div>
-		</nav>
+		</div>
 	);
 };
